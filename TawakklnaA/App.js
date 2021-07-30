c9277aa28fd85d5b70155a0769c5e9a9c142f671
Younes Alturkey <younes.alturkey@tuwaiq.edu.sa>
2021-07-30 20:01:46 +0300

    add: warning section and loading facade

--- TawakklnaA/App.js
+++ TawakklnaA/App.js
@@ -1,74 +1,84 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Image, View } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import Home from "./screens/Home";
-import Profile from "./screens/Profile";
-import Services from "./screens/Services";
-import Ionicons from "react-native-vector-icons/Ionicons";
+import React, { useEffect, useState } from 'react'
+import { ActivityIndicator, Image, View } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import Home from './screens/Home'
+import Profile from './screens/Profile'
+import Services from './screens/Services'
+import Ionicons from 'react-native-vector-icons/Ionicons'
 
-const Tab = createBottomTabNavigator();
+const Tab = createBottomTabNavigator()
 export default function App() {
+  const [loading, setLoading] = useState(() => true)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false)
+    }, 3000)
+  })
+
+  if (loading) {
+    return (
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: '#121212',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <ActivityIndicator size="large" color="#23AA9C" />
+      </View>
+    )
+  }
   return (
     <NavigationContainer>
       <Tab.Navigator
-        tabBarOptions={{
-          activeTintColor: "#23AA9C",
-          inactiveTintColor: "#516477",
-        }}
         screenOptions={({ route }) => ({
           tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
+            let iconName
 
-            if (route.name === "Home") {
+            if (route.name === 'Home') {
               iconName = focused
-                ? require("./assets/icons/active/home.png")
-                : require("./assets/icons/notActive/home.png");
-            } else if (route.name === "Profile") {
+                ? require('./assets/icons/active/home.png')
+                : require('./assets/icons/notActive/home-gray.png')
+            } else if (route.name === 'Profile') {
               iconName = focused
-                ? require("./assets/icons/active/myAccount.png")
-                : require("./assets/icons/notActive/myAccount.png");
-            } else if (route.name === "Services") {
+                ? require('./assets/icons/active/myAccount.png')
+                : require('./assets/icons/notActive/myAccount-gray.png')
+            } else if (route.name === 'Services') {
               iconName = focused
-                ? require("./assets/icons/active/services.png")
-                : require("./assets/icons/notActive/services.png");
-            } else if (route.name === "Dashboard") {
+                ? require('./assets/icons/active/services.png')
+                : require('./assets/icons/notActive/services-gray.png')
+            } else if (route.name === 'Dashboard') {
               iconName = focused
-                ? require("./assets/icons/active/dashboard.png")
-                : require("./assets/icons/notActive/dashboard.png");
-            } else if (route.name === "Wallet") {
+                ? require('./assets/icons/active/dashboard.png')
+                : require('./assets/icons/notActive/dashboard-gray.png')
+            } else if (route.name === 'Wallet') {
               iconName = focused
-                ? require("./assets/icons/active/wallet.png")
-                : require("./assets/icons/notActive/wallet.png");
+                ? require('./assets/icons/active/wallet.png')
+                : require('./assets/icons/notActive/wallet-gray.png')
             }
 
             // You can return any component that you like here!
-            return (
-              <Image source={iconName} style={{ width: 25, height: 25 }} />
-            );
+            return <Image source={iconName} style={{ width: 25, height: 25 }} />
           },
         })}
         tabBarOptions={{
-          activeTintColor: "tomato",
-          inactiveTintColor: "gray",
+          activeTintColor: '#23AA9C',
+          inactiveTintColor: '#AFAFAF',
+          style: {
+            backgroundColor: '#191b1c',
+            borderTopWidth: 0,
+          },
         }}
       >
         <Tab.Screen name="Home" component={Home} />
-        <Tab.Screen name="Profile" component={Profile} />
         <Tab.Screen name="Services" component={Services} />
-        <Tab.Screen name="Dashboard" component={Home} />
         <Tab.Screen name="Wallet" component={Home} />
+        <Tab.Screen name="Dashboard" component={Home} />
+        <Tab.Screen name="Profile" component={Profile} />
       </Tab.Navigator>
     </NavigationContainer>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
